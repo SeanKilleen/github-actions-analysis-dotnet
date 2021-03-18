b07eb91dc18f95a30f32785daaffa1184ad2e739
Sean Killeen <SeanKilleen@gmail.com>
2021-03-18 16:03:55 -0400

    Basic setup
    * Default web app
    * Test project (NUnit 3)
    * Calculator + CalculatorTests

---
+++ src/MyDemoApp/MyDemoApp.Tests/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyDemoApp.Tests
+{
+
+    public class CalculatorTests
+    {
+        [Test]
+        public void MathStillWorks()
+        {
+            var result = 1 + 1;
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ src/MyDemoApp/MyDemoApp.Web/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDemoApp.Web
+{
+    public class Calculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
